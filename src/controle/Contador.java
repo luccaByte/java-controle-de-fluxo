c8250ab4c14504c19dd678f981450316a85053db
lucas <lucaston2323@gmail.com>
2023-10-11 21:39:41 -0300

    Criando um contador com tratamento de exceções

--- src/controle/Contador.java
+++ src/controle/Contador.java
@@ -1,26 +1,45 @@
 package controle;
 
+import java.util.Scanner;
+
 public class Contador {
     public static void main(String[] args) {
 		Scanner terminal = new Scanner(System.in);
-		System.out.println("Digite o primeiro parâmetro");
-		int parametroUm = terminal.??;
-		System.out.println("Digite o segundo parâmetro");
-		int parametroDois = terminal.??;
+
+            System.out.println("Digite o primeiro parâmetro: ");
+            int parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo parâmetro:");
+            int parametroDois = terminal.nextInt();
+            terminal.close();
 		
 		try {
 			//chamando o método contendo a lógica de contagem
-			contar(parametroUm, parametroDois);
+			contar (parametroUm, parametroDois);
 		
-		}catch (? exception) {
+		}catch (Exception e) {
 			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+            System.out.println(e.getMessage());
 		}
-		
 	}
+    
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-		
-		int contagem = parametroDois - parametroUm;
-		//realizar o for para imprimir os números com base na variável contagem
+        if (parametroUm > parametroDois) {
+            try {
+                throw new Exception (" segundo parâmetro deve ser maior que o primeiro");
+            } catch (Exception e) {
+                
+            }
+        } else {
+            int contagem = parametroDois - parametroUm;
+            if (contagem < 0) {
+                System.out.println("Não há números para imprimir.");
+            } else {
+                //realizar o for para imprimir os números com base na variável contagem
+                for (int i = parametroUm; i <= parametroDois; i++) {
+                    System.out.println("Imprimindo o número " + i);
+                }
+            }
+        }
 	}
 }
